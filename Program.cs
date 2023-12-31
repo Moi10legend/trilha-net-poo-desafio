1da06444322827d61bc1bd3613474be6b161ad8c
Moi10legend <moisesflopesf@gmail.com>
2023-12-31 17:13:21 -0300

    Desafio de projeto concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia tijolao = new Nokia("90028922", "tijolao", "1111111", 1000);
+tijolao.Ligar();
+tijolao.ReceberLigacao();
+tijolao.InstalarAplicativo("Instagram");
+
+Iphone iphone15 = new Iphone("99387236", "Iphone 15", "12323", 128);
+iphone15.Ligar();
+iphone15.ReceberLigacao();
+iphone15.InstalarAplicativo("Google");
